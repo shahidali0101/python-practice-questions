73f31e9edd46ac89575d6c508135f4f174cd7a8c
Shahid Ali <shahids.user@gmail.com>
2026-01-27 00:42:21 +0530

    Try luck

---
+++ mini_projects/lottery_game.py
@@ -0,0 +1,23 @@
+
+
+import random
+import time
+
+sct = random.randint(1, 100)
+
+print("Welcome to Lottery Game!")
+time.sleep(1)
+
+for i in range(5):
+    user = int(input("Please enter a number: "))
+    print("Checking your number...")
+    time.sleep(1.5)
+
+    if user == sct:
+        print("🎉 You won the lottery!")
+        break
+    else:
+        print("❌ Better luck next time")
+        time.sleep(1)
+
+print("🏆 Lottery number was:", sct)
