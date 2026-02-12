6649269c7ad7a256eae496d4a0b21284d9a4ff62
Shahid Ali <shahids.user@gmail.com>
2026-02-13 00:19:19 +0530

    Guessing game

---
+++ mini_projects/Number_Guessing_Game.py
@@ -0,0 +1,40 @@
+"""
+Number Guessing Game
+
+Description:
+A simple command-line game where the user tries to guess
+a randomly generated number between 1 and 50.
+
+Features:
+- Random number generation
+- Hint system (higher/lower)
+- Attempt counter
+
+"""
+
+import random
+
+# Generate random number
+JACKPOT_NUMBER = random.randint(1, 50)
+
+# Attempt counter
+attempts = 1
+
+# User input
+guess = int(input("Enter a number between 1 and 50: "))
+
+# Game loop
+while guess != JACKPOT_NUMBER:
+
+    if guess < JACKPOT_NUMBER:
+        print("Guess higher ")
+    else:
+        print("Guess lower ")
+
+    guess = int(input("Enter a number between 1 and 50: "))
+    attempts += 1
+
+# Win message
+print("\n🎉 Congratulations! You guessed it right!")
+print(f"You took {attempts} attempt(s) to win.")
+
